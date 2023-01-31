d813de787d18c1d4bc300a287f614c5f7a3982a5
andreenakashima <andreenakashima@gmail.com>
2023-01-31 14:57:42 -0300

    Adicionado evento de click

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
 export function App() {
-	return <h1 className="bg-slate-400">teste</h1>;
+	function handleClick(event) {
+		console.log(event);
+	}
+
+	return (
+		<div
+			className="w-screen h-screen bg-slate-200 relative"
+			onClick={handleClick}
+		>
+			<span className="absolute top-32 left-32 w-2 h-2 block  bg-red-800 rounded-full" />
+		</div>
+	);
 }
